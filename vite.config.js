7beeacfabb160d0b04f7053c3de735e2027bd5d9
sreevalli <valliwebworkz@gmail.com>
2025-05-27 21:50:38 +0530

    Update vite.config.js with base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Add this 'base' property:
+  base: "/album/", // <--- ADD THIS LINE with your repository name and trailing slash
   plugins: [react()],
-})
+});
